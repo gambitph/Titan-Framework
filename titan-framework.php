74a5683f17c17a619e405e14699b252bf14b257e
bfintal <bfintal@gmail.com>
2014-02-19 20:36:22 +0800

    Forgot to update the plugin version @_@

--- titan-framework.php
+++ titan-framework.php
@@ -4,7 +4,7 @@ Plugin Name: Titan Framework
 Plugin URI: http://titanframework.net/
 Description: Titan Framework allows theme and plugin developers to create a admin pages, options, meta boxes, and theme customizer options with just a few simple lines of code.
 Author: Benjamin Intal, Gambit
-Version: 1.0
+Version: 1.0.2
 Author URI: http://gambit.ph
 */
 
