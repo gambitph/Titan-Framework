0b6146b7ac724fb2369a5d383a81bc223d7bf2fa
bfintal <bfintal@gmail.com>
2014-03-01 23:02:38 +0800

    Testing Travis

--- tests/test_wordpress_plugin_tests.php
+++ tests/test_wordpress_plugin_tests.php
@@ -17,30 +17,6 @@ class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
 	 }
 
 	/**
-	 * If these tests are being run on Travis CI, verify that the version of
-	 * WordPress installed is the version that we requested.
-	 *
-	 * @requires PHP 5.3
-	 */
-	function test_wp_version() {
-
-		if ( !getenv( 'TRAVIS' ) )
-			$this->markTestSkipped( 'Test skipped since Travis CI was not detected.' );
-
-		$requested_version = getenv( 'WP_VERSION' ) . '-src';
-
-		// The "master" version requires special handling.
-		if ( $requested_version == 'master-src' ) {
-			$file = file_get_contents( 'https://raw.github.com/tierra/wordpress/master/src/wp-includes/version.php' );
-			preg_match( '#\$wp_version = \'([^\']+)\';#', $file, $matches );
-			$requested_version = $matches[1];
-		}
-
-		$this->assertEquals( get_bloginfo( 'version' ), $requested_version );
-
-	}
-
-	/**
 	 * Ensure that the plugin has been installed and activated.
 	 */
 	function test_plugin_activated() {
