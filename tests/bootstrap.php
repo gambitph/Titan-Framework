7110e43faeddf5a5ac94f1ab3159004b9747bc3e
bfintal <bfintal@gmail.com>
2014-03-01 23:38:21 +0800

    Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,19 +7,18 @@
  * @package wordpress-plugin-tests
  */
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+// $_tests_dir = getenv('WP_TESTS_DIR');
+// if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+//
+// require_once $_tests_dir . '/includes/functions.php';
+//
+// function _manually_load_plugin() {
+// 	require dirname( __FILE__ ) . '/../titan-framework.php';
+// }
+// tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+//
+// require $_tests_dir . '/includes/bootstrap.php';
 
-require_once $_tests_dir . '/includes/functions.php';
-
-function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../titan-framework.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-require $_tests_dir . '/includes/bootstrap.php';
-
-/*
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'Titan-Framework/titan-framework.php' ),
@@ -29,9 +28,8 @@ $GLOBALS['wp_tests_options'] = array(
 // location. Otherwise, we'll just assume that this plugin is installed in a
 // WordPress develop SVN checkout.
 
-if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
-} else {
-	require '../../../../tests/phpunit/includes/bootstrap.php';
-}
-*/
+// if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	// require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
+// } else {
+	require '/tmp/wordpress/tests/phpunit/includes/bootstrap.php';
+// }
