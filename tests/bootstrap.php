d73723f3bf2b8d6679454204a1bdd4dd50266090
bfintal <bfintal@gmail.com>
2014-03-02 01:05:07 +0800

    Finally Travis is Working

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Bootstrap the plugin unit testing environment.
- *
- * Edit 'active_plugins' setting below to point to your main plugin file.
- *
- * @package wordpress-plugin-tests
- */
 
 $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'Titan-Framework/titan-framework.php' ),
@@ -14,30 +7,4 @@ $GLOBALS['wp_tests_options'] = array(
 $_tests_dir = getenv('WP_TESTS_DIR');
 if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
-// require_once $_tests_dir . '/includes/functions.php';
-//
-// function _manually_load_plugin() {
-// 	require dirname( __FILE__ ) . '/../titan-framework.php';
-// }
-// tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
 require $_tests_dir . '/includes/bootstrap.php';
-
-
-
-/*
-// Activates this plugin in WordPress so it can be tested.
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'Titan-Framework/titan-framework.php' ),
-);
-
-// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
-// location. Otherwise, we'll just assume that this plugin is installed in a
-// WordPress develop SVN checkout.
-
-if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
-} else {
-	require '/tmp/wordpress/tests/phpunit/includes/bootstrap.php';
-}
-*/
