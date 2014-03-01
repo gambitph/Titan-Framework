f0ed473ddffa6902407b1ee4261984ab69cada96
bfintal <bfintal@gmail.com>
2014-03-01 23:08:01 +0800

    Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,19 @@
  * @package wordpress-plugin-tests
  */
 
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+
+require_once $_tests_dir . '/includes/functions.php';
+
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../titan-framework.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require $_tests_dir . '/includes/bootstrap.php';
+
+/*
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'Titan-Framework/titan-framework.php' ),
@@ -21,3 +34,4 @@ if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
 } else {
 	require '../../../../tests/phpunit/includes/bootstrap.php';
 }
+*/
