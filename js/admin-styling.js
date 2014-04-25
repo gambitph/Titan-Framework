e23065d277d98dccf2e5326ab084bd3e69c259ae
Sean McCafferty <smccafferty@voceconnect.com>
2014-04-25 12:47:57 -0400

    Adding the form-table class when selecting the tr to remove to not potentially interfere with custom types.

--- js/admin-styling.js
+++ js/admin-styling.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
 
 	// Removes the last blank table in admin pages. This is because a blank table is left if the last
 	// option is a save option
-	$('.titan-framework-panel-wrap table').filter(function() {
+	$('.titan-framework-panel-wrap table.form-table').filter(function() {
 		return $(this).find('tbody tr').length == 0;
 	}).remove();
 });
