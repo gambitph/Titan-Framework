917a2131ca6ab00dec8e4fe6850abccd130a2354
Benjamin Intal <bfintal@gmail.com>
2014-08-03 14:54:17 +0800

    v1.6-dev

--- titan-framework.php
+++ titan-framework.php
@@ -4,7 +4,7 @@ Plugin Name: Titan Framework
 Plugin URI: http://www.titanframework.net/
 Description: Titan Framework allows theme and plugin developers to create a admin pages, options, meta boxes, and theme customizer options with just a few simple lines of code.
 Author: Benjamin Intal, Gambit
-Version: 1.5.2
+Version: 1.6-dev
 Author URI: http://gambit.ph
 */
 
