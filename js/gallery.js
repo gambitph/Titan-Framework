2e2ae411ae9b20eefd2ce7ce95a9f1c51970a207
Hasin Hayder <hasin@leevio.com>
2014-09-14 02:37:01 +0600

    Gallery Placeholder

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,19 @@
 (function($){
+    var file_frame,data,ats;
+    ats = [];
     $(document).ready(function(){
         alert("hi");
     });
 
+
+
     $("#galgal").on("click",function(){
+
+        if ( file_frame ) {
+            file_frame.open();
+            return;
+        }
+
         file_frame = wp.media.frames.file_frame = wp.media({
             frame:    'post',
             state:    'insert',
@@ -19,13 +29,33 @@
          * to the insert event.
          */
         file_frame.on( 'insert', function() {
-
             /**
              * We'll cover this in the next version.
              */
-            var data = file_frame.state().get( 'selection' ).toJSON();
-            console.log(data);
+            var data = file_frame.state().get( 'selection' );
+            alert("ss");
+            data.map( function( attachment ) {
 
+                //console.log(attachment);
+                ats.push(attachment.id);
+                console.log(attachment.attributes);
+                attachment = attachment.toJSON();
+
+                // Do something with attachment.id and/or attachment.url here
+            });
+            var jdata = data.toJSON();
+
+            console.log(jdata);
+
+        });
+
+        file_frame.on('open', function(){
+            var selection = file_frame.state().get('selection');
+
+            if (ats.length>0) {
+                for(i in ats)
+                selection.add(wp.media.attachment(ats[i]));
+            }
         });
 
         // Now display the actual file_frame
