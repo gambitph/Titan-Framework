ed1eda0e6878bcdf47277b6b0fcea92c8c92dced
Benjamin Intal <bfintal@gmail.com>
2015-09-10 12:00:11 +0800

    Updated docs

--- lib/class-option-heading.php
+++ lib/class-option-heading.php
@@ -1,10 +1,21 @@
 <?php
+/**
+ * Heading option
+ *
+ * @package Titan Framework
+ */
 
-if ( ! defined( 'ABSPATH' ) ) { exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) { exit; // Exit if accessed directly.
 }
+
+/**
+ * Heading option class
+ *
+ * @since 1.0
+ */
 class TitanFrameworkOptionHeading extends TitanFrameworkOption {
 
-	/*
+	/**
 	 * Display for options and meta
 	 */
 	public function display() {
@@ -22,9 +33,13 @@ class TitanFrameworkOptionHeading extends TitanFrameworkOption {
 		<?php
 	}
 
-	/*
-	* Display for theme customizer
-	*/
+	/**
+	 * Display for theme customizer
+	 *
+	 * @param WP_Customize                         $wp_customize The customizer object.
+	 * @param TitanFrameworkThemeCustomizerSection $section      The customizer section.
+	 * @param int                                  $priority     The display priority of the control.
+	 */
 	public function registerCustomizerControl( $wp_customize, $section, $priority = 1 ) {
 		$wp_customize->add_control( new TitanFrameworkOptionHeadingControl( $wp_customize, $this->getID(), array(
 			'label' => $this->settings['name'],
@@ -38,25 +53,39 @@ class TitanFrameworkOptionHeading extends TitanFrameworkOption {
 }
 
 
-/*
- * We create a new control for the theme customizer
+// We create a new control for the theme customizer.
+add_action( 'customize_register', 'register_titan_framework_option_heading_control', 1 );
+
+/**
+ * Register the customizer control
  */
-add_action( 'customize_register', 'registerTitanFrameworkOptionHeadingControl', 1 );
-function registerTitanFrameworkOptionHeadingControl() {
+function register_titan_framework_option_heading_control() {
+
+	/**
+	 * Heading option class
+	 *
+	 * @since 1.0
+	 */
 	class TitanFrameworkOptionHeadingControl extends WP_Customize_Control {
+
+		/**
+		 * The description of this control
+		 *
+		 * @var bool
+		 */
 		public $description;
 
+		/**
+		 * Renders the control
+		 */
 		public function render_content() {
 
-			?><span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span><?php
+			?><span class="customize-control-title"><?php echo esc_html( $this->label ) ?></span><?php
 
 if ( ! empty( $this->description ) ) {
-	echo "<p class='description'>" . $this->description . '</p>';
+	echo "<p class='description'>" . wp_kses_post( $this->description ) . '</p>';
 }
 
-				?>
-
-				<?php
 		}
 	}
 }
