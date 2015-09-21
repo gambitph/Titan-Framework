9ef4bf8a708cb2b638d3b3a96d8ba28d020cc72c
Benjamin Intal <bfintal@gmail.com>
2015-09-21 16:59:17 +0800

    Added text option test

---
+++ tests/test-option-text.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Tests for class-option-text.php
+ *
+ * @package Titan Framework Tests
+ */
+
+class Titan_Framework_Option_Text_Test extends WP_UnitTestCase {
+	
+	public $adminOption;
+	public $customizerOption;
+	public $metaOption;
+	
+	public function setUp() {
+		
+		add_action( 'tf_create_options', array( $this, 'tf_create_options' ) );
+		do_action( 'init' );
+		
+		parent::setUp();
+	}
+	
+	public function tearDown() {		
+		remove_action( 'tf_create_options', array( $this, 'tf_create_options' ) );
+		
+		parent::tearDown();
+	}
+	
+	public function tf_create_options() {
+		$titan = TitanFramework::getInstance( 'testing' );
+		
+		$container = $titan->createAdminPage( array(
+			'name' => 'test container',
+		) );
+		$this->adminOption = $container->createOption( array(
+			'id' => 'test1',
+			'type' => 'text',
+			'default' => 'default',
+		) );
+		
+
+		$container = $titan->createCustomizer( array(
+			'name' => 'test container',
+		) );
+		$this->customizerOption = $container->createOption( array(
+			'id' => 'test2',
+			'type' => 'text',
+			'default' => 'default',
+		) );
+		
+
+		$container = $titan->createMetaBox( array(
+			'name' => 'test container',
+		) );
+		$this->metaOption = $container->createOption( array(
+			'id' => 'test3',
+			'type' => 'text',
+			'default' => 'default',
+		) );
+	}
+	
+	function test_option_save_get_admin_page() {
+		$option = $this->adminOption;
+		
+		$titan = TitanFramework::getInstance( 'testing' );
+		
+		$id = $option->settings['id'];
+		$this->assertEquals( 'default', $titan->getOption( $id ) );
+		
+		$option->setValue( 'modified' );
+		$titan->saveInternalAdminPageOptions();
+		$this->assertEquals( 'modified', $titan->getOption( $id ) );
+		
+		$option->setValue( 'modified again' );
+		$titan->saveInternalAdminPageOptions();
+		$this->assertEquals( 'modified again', $titan->getOption( $id ) );
+		
+		$option->setValue( '文字化け€àáßèëœ蟄怜喧らاة على هذاรื่องร로그ĐứЯрополкمارسאבגדடாஹ்கோ' );
+		$titan->saveInternalAdminPageOptions();
+		$this->assertEquals( '文字化け€àáßèëœ蟄怜喧らاة على هذاรื่องร로그ĐứЯрополкمارسאבגדடாஹ்கோ', $titan->getOption( $id ) );
+	}
+	
+	function test_option_save_get_customizer() {
+		$option = $this->customizerOption;
+		
+		$titan = TitanFramework::getInstance( 'testing' );
+		
+		$id = $option->settings['id'];
+		$this->assertEquals( 'default', $titan->getOption( $id ) );
+		
+		$option->setValue( 'modified' );
+		$this->assertEquals( 'modified', $titan->getOption( $id ) );
+		
+		$option->setValue( '文字化け€àáßèëœ蟄怜喧らاة على هذاรื่องร로그ĐứЯрополкمارسאבגדடாஹ்கோ' );
+		$this->assertEquals( '文字化け€àáßèëœ蟄怜喧らاة على هذاรื่องร로그ĐứЯрополкمارسאבגדடாஹ்கோ', $titan->getOption( $id ) );
+	}
+	
+	function test_option_save_get_meta_box() {
+		$option = $this->metaOption;
+		
+		$titan = TitanFramework::getInstance( 'testing' );
+
+		$p = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
+		
+		global $post;
+		$post = get_post( $p );
+		setup_postdata( $post );
+		
+		$id = $option->settings['id'];
+		$this->assertEquals( 'default', $titan->getOption( $id ) );
+		
+		$option->setValue( 'modified' );
+		$this->assertEquals( 'modified', $titan->getOption( $id ) );
+		
+		$option->setValue( '文字化け€àáßèëœ蟄怜喧らاة على هذاรื่องร로그ĐứЯрополкمارسאבגדடாஹ்கோ' );
+		$this->assertEquals( '文字化け€àáßèëœ蟄怜喧らاة على هذاรื่องร로그ĐứЯрополкمارسאבגדடாஹ்கோ', $titan->getOption( $id ) );
+		
+	}
+	
+}
+
