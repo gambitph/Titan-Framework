bc685e73ee4c7428766107ee4913f3d695dd0d6e
Benjamin Intal <bfintal@gmail.com>
2015-09-21 12:56:06 +0800

    Adjusted tests to fit function changes

--- tests/test-titan-framework.php
+++ tests/test-titan-framework.php
@@ -7,10 +7,8 @@
 
 class Titan_Framework_Test extends PHPUnit_Framework_TestCase {
 	
-	public $tf_initializing = false;
 	public $tf_create_options_called = false;
 	public function tf_create_options() {
-		$this->tf_initializing = TitanFramework::$initializing;
 		$this->tf_create_options_called = true;
 	}
 	public $tf_done_called = false;
@@ -18,38 +16,18 @@ class Titan_Framework_Test extends PHPUnit_Framework_TestCase {
 		$this->tf_done_called = true;
 	}
 	
-	function test_trigger_initial_option_creation() {
-		$o = new TitanFrameworkPlugin();
-		
-		$this->tf_initializing = false;
-		$this->tf_create_options_called = false;
-		$this->tf_done_called = false;
-		
-		add_action( 'tf_create_options', array( $this, 'tf_create_options' ) );
-		add_action( 'tf_done', array( $this, 'tf_done' ) );
-		
-		$o->trigger_initial_option_creation();
-		$this->assertTrue( $this->tf_create_options_called );
-		$this->assertTrue( $this->tf_initializing );
-		
-		remove_action( 'tf_create_options', array( $this, 'tf_create_options' ) );
-		remove_action( 'tf_done', array( $this, 'tf_done' ) );
-	}
-	
 	function test_trigger_actual_option_creation() {
 		$o = new TitanFrameworkPlugin();
 		
-		$this->tf_initializing = false;
 		$this->tf_create_options_called = false;
 		$this->tf_done_called = false;
 		
 		add_action( 'tf_create_options', array( $this, 'tf_create_options' ) );
 		add_action( 'tf_done', array( $this, 'tf_done' ) );
 		
-		$o->trigger_actual_option_creation();
+		$o->trigger_option_creation();
 		$this->assertTrue( $this->tf_create_options_called );
 		$this->assertTrue( $this->tf_done_called );
-		$this->assertFalse( $this->tf_initializing );
 		
 		remove_action( 'tf_create_options', array( $this, 'tf_create_options' ) );
 		remove_action( 'tf_done', array( $this, 'tf_done' ) );
