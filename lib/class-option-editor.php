b12b533d804141335d2c6d46b72252998377cae1
Benjamin Intal <bfintal@gmail.com>
2015-09-23 19:46:05 +0800

    Added `editor_settings` parameter
    Closes #223

--- lib/class-option-editor.php
+++ lib/class-option-editor.php
@@ -8,19 +8,27 @@ class TitanFrameworkOptionEditor extends TitanFrameworkOption {
 		'wpautop' => true,
 		'media_buttons' => true,
 		'rows' => 10,
+		'editor_settings' => array(),
 	);
 
 	/*
 	 * Display for options and meta
 	 */
 	public function display() {
-		$this->echoOptionHeader();
-
-		wp_editor( $this->getValue(), $this->getID(), array(
+		
+		$editorSettings = array(
 			'wpautop' => $this->settings['wpautop'],
 			'media_buttons' => $this->settings['media_buttons'],
 			'textarea_rows' => $this->settings['rows'],
-		) );
+		);
+		
+		if ( is_array( $this->settings['editor_settings'] ) ) {
+			$editorSettings = array_merge( $editorSettings, $this->settings['editor_settings'] );
+		}
+		
+		$this->echoOptionHeader();
+
+		wp_editor( $this->getValue(), $this->getID(), $editorSettings );
 
 		$this->echoOptionFooter();
 	}
